78d964daa8959d2ecc21cea952f130a4bb306c3b
Berkay Anduv <sygrok@hotmail.com>
2024-01-08 11:47:52 +0300

    Install

--- README.md
+++ README.md
@@ -1,1 +1,110 @@
-#Typescript
+# Typescript
+
+for Angular Typescript
+[GitHub](https://https://github.com/sygrok/Typescript-Section-TechcareerBootcamp)
+
+---
+
+## Git Terminal Codes
+
+```sh
+git init
+git add .
+git commit -m "init git"
+git add remote origin44 URL
+git branch -M main
+git push -u origin44  main
+ssh-keygen -t rsa -b 4096 -C "hamitmizrak@gmail.com"
+cat ~/.ssh/id_rsa.pub ==> KOPYALAA GİTHUB SSH YAPIŞTIR.
+```
+
+## Npm Codes
+
+```sh
+npm install
+npm i
+
+npm list
+npm list -g
+npm list -g --depth=0
+npm root
+npm root -g
+
+npm search express
+
+npm install express --save
+npm install -g express
+npm install mocha --save-dev
+npm install tailwindcss -D
+
+npm uninstall express
+
+npm update
+npm update express
+```
+
+---
+
+## Linux
+
+```sh
+ls -la
+cd .
+cd ..
+mkdir dizin
+cat >> deneme.tx
+```
+
+## Package Json
+
+```sh
+node -v  => v20.10.0 (Semantic Version)
+npm -v   => 10.2.3   (Semantic Version)
+npm init
+npm init -y
+npm install
+```
+
+---
+
+## Nodemon (Nodemon: Node js monitor => Javascript sürekli takip eder)
+
+```sh
+npm install -g nodemon --save
+npm install  nodemon --save-dev
+nodemon index.js
+nodemon -q index.js
+```
+
+---
+
+## Typescript
+
+```sh
+npm install -g typescript
+npm install typescript --save-dev
+
+tsc index.ts
+
+tsc -w index.ts
+nodemon index.js
+```
+
+---
+
+## Web Pack
+
+```sh
+
+```
+
+---
+
+## RUN
+
+```sh
+npm run typescript
+npm run nodemon
+```
+
+---
---
+++ src/index.js
@@ -0,0 +1,110 @@
+// Javascript Nedir ?
+/*
+Betik dildir.
+Tarayıcı tarafında çalışır.
+Nodejs ile sunucu tarafında da çalışır.
+OOP (interface,inheritance,abstract)
+*/
+// TypeScript Nedir ?
+/*
+TS: Açık kaynak kodlu bir dildir.
+TS: Esnektir.
+TS: Js herşeyi kullanabilirsiniz.
+TS: Microsoft ürünüdür. C# , Hejlberg
+TS: Javascript+...+ diğer özellikleri
+Javascript = TS
+
+TS: Esnektir
+TS: Static type
+TS: Derleme zamanı
+TS: Yazılım kalitesini arttırır.
+TS: Hata bulma kolaylığı
+TS: Kod okunabilirliği
+TS: Büyük projelerde kullanılır.
+TS: Angular,React JS,Vue ile geliştirmeler yapabiliriz.
+TS: Nodejs ile backend tarafında da kullanabiliriz.
+TS: Javascript ile çalışır.
+TS: Javascript kodlarını derler.
+TS: Javascript'e dönüştürür.
+
+
+*/
+// TypeScript Özellikleri Nelerdir ?
+/*
+
+*/
+// TypeScript Neden Öğrenmeliyim ?
+/*
+
+*/
+// TypeScript Genel Bileşenleri Nelerdir ?
+/*
+
+*/
+// Ecmascript ile JavaScript Arasındaki Farklar Nelerdir ?
+/*
+
+*/
+// TypeScript ile JavaScript Arasındaki Farklar Nelerdir ?
+/*
+
+*/
+// TypeScript ile Ecmascript Arasındaki Farklar Nelerdir ?
+/*
+
+*/
+// TypeScript Kurulumu
+/*
+
+*/
+// TypeScript Compiler
+// Javascript Interpreter
+// Java: Interpreter and Compiler
+// #### #################################################
+// TypeScript Editor
+// TypeScript İlk Program
+// TypeScript Değişkenler
+// TypeScript Veri Tipleri
+// TypeScript Operatörler
+// TypeScript Koşullu İfadeler
+// TypeScript Döngüler
+// TypeScript Fonksiyonlar
+// TypeScript Sınıflar
+// TypeScript Arayüzler
+// TypeScript Diziler
+// TypeScript Tuple
+// TypeScript Enum
+// TypeScript Any
+// TypeScript Void
+// TypeScript Null ve Undefined
+// TypeScript Never
+// TypeScript Type Assertion
+// TypeScript Type Aliases
+// TypeScript String Methods
+// TypeScript Number Methods
+// TypeScript Array Methods
+// TypeScript Object Methods
+// TypeScript Tarih Metodları
+// TypeScript Math Metodları
+// TypeScript Regular Expressions
+// TypeScript Template Literals
+// TypeScript Decorators
+// TypeScript Mixins
+// TypeScript Interfaces vs Types
+// TypeScript Modules
+// TypeScript Namespaces
+// TypeScript Namespaces vs Modules
+// TypeScript Ambient Namespaces
+// TypeScript Ambient Modules
+// TypeScript Generics
+// TypeScript Generic Constraints
+// TypeScript Generic Classes
+// TypeScript Generic Interfaces
+// TypeScript Generic Functions
+// TypeScript Generic Enums
+// TypeScript Generic Type Aliases
+// TypeScript Generic Utility Types
+// TypeScript Inference
+// TypeScript Type Compatibility
+// TypeScript Advanced Types
+// TypeScript Intersection Types
